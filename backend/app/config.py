bc6b973e49cde938baaad47222259915c227717c
Ayush Deshmukh <deshmukhayush26@gmail.com>
2026-01-28 18:37:08 +0530

    Add JWT access token expiration setting

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,9 @@
 import os
+from datetime import timedelta
+
 class Config:
+
     SQLALCHEMY_DATABASE_URI = 'sqlite:///insti_forum.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=48)
